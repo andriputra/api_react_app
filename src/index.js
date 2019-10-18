505df8411066fe0ba5b8dc54243ce5bd53d4e3b5
andriputra <andriputra.job@gmail.com>
2019-10-18 22:44:06 +0700

    move style to assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-import "./style.scss";
-import "./style-responsive.scss";
+import "./assets/style/style.scss";
+import "./assets/style/style-responsive.scss";
 
 ReactDOM.render(
   <Main/>,
